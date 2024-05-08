7fc42f9dccb398c893358a9e05a7635946eef427
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-08 22:02:37 +0000

    creating the SignUp form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-api-p5-76a52f692867.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
