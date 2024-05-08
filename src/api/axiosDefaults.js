a16736ee4397c3d865264ae475abeae39c19265c
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-08 22:21:33 +0000

    Add access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-p5-76a52f692867.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
